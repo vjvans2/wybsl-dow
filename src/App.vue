9b714e3d36b4f3ff32e58001dad01a4e1c00f557
Vinny <vincent.vanslyke@gmail.com>
2025-03-22 14:04:40 -0400

    good to go

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <h1>Birthdate Checker</h1>
-    <fieldset style="max-width:300px">
+    <h1>League Day of Week Checker</h1>
+    <fieldset style="max-width: 300px">
       <legend>Select your gender:</legend>
       <label>
         <input type="radio" value="male" v-model="gender" />
@@ -12,20 +12,98 @@
         Female
       </label>
     </fieldset>
-<br>
+    <br />
+    <label v-if="gender"
+      ><input
+        type="checkbox"
+        :checked="pick === 'tb_one'"
+        @change="getDow('tb_one')"
+      />Tee Ball I</label
+    ><br />
+    <label v-if="gender"
+      ><input
+        type="checkbox"
+        :checked="pick === 'tb_two'"
+        @change="getDow('tb_two')"
+      />Tee Ball II</label
+    ><br />
+    <div v-if="gender === 'male'">
+      <label
+        ><input
+          type="checkbox"
+          :checked="pick === '8u_baseball'"
+          @change="getDow('8u_baseball')"
+        />8U Coach Pitch Baseball</label
+      ><br />
+      <label
+        ><input
+          type="checkbox"
+          :checked="pick === '10u_baseball'"
+          @change="getDow('10u_baseball')"
+        />10U Kid Pitch Baseball</label
+      ><br />
+      <label
+        ><input
+          type="checkbox"
+          :checked="pick === '12u_baseball'"
+          @change="getDow('12u_baseball')"
+        />12U Kid Pitch Baseball</label
+      ><br />
+      <label
+        ><input
+          type="checkbox"
+          :checked="pick === '14u_baseball'"
+          @change="getDow('14u_baseball')"
+        />14U Kid Pitch Baseball</label
+      ><br />
+      <label
+        ><input
+          type="checkbox"
+          :checked="pick === '18u_baseball'"
+          @change="getDow('18u_baseball')"
+        />18U Kid Pitch Baseball</label
+      ><br />
+    </div>
+    <div v-if="gender === 'female'">
+      <label
+        ><input
+          type="checkbox"
+          :checked="pick === '8u_softball'"
+          @change="getDow('8u_softball')"
+        />8U Coach Pitch Softball</label
+      ><br />
+      <label
+        ><input
+          type="checkbox"
+          :checked="pick === '10u_softball'"
+          @change="getDow('10u_softball')"
+        />10U Kid Pitch Softball</label
+      ><br />
+      <label
+        ><input
+          type="checkbox"
+          :checked="pick === '12u_softball'"
+          @change="getDow('12u_softball')"
+        />12U Kid Pitch Softball</label
+      ><br />
+      <label
+        ><input
+          type="checkbox"
+          :checked="pick === '17u_softball'"
+          @change="getDow('17u_softball')"
+        />17U Kid Pitch Softball</label
+      ><br />
+    </div>
 
-    <!-- put gender dependent checkboxes for leagues here -->
-
-    <hr v-if="resultText">
+    <hr v-if="resultText" />
     <div v-if="resultText" class="resultText">
       {{ resultText }}
     </div>
-    
   </div>
 </template>
 
 <script>
-import { ref, computed, watch } from "vue";
+import { ref, watch } from "vue";
 
 export default {
   setup() {
@@ -33,55 +111,51 @@ export default {
     const pick = ref("");
     const resultText = ref("");
 
-    watch(pick, () => {
-      resultText.value = "";
-    });
-
     watch(gender, () => {
       pick.value = "";
       resultText.value = "";
-    })
-
-    const getLeagueDowText = (pick, gender) => {
-      if (gender === 'male') {
-        switch (pick) {
-          case 4: return 'Tee Ball 1';
-          case 5: return 'Tee Ball 1';
-          case 6: return 'Tee Ball 2';
-          case 7: return 'Coach Pitch Baseball (8U)';
-          case 8: return 'Coach Pitch Baseball (8U)';
-          default: return ''
-        }
-      } else {
-        switch (playingAge) {
-          case 4: return 'Tee Ball 1';
-          case 5: return 'Tee Ball 1';
-          case 6: return 'Tee Ball 2';
-          case 7: return 'Coach Pitch Softball (8U)';
-          case 8: return 'Coach Pitch Softball (8U)';
-          default: return ''
-        }
-      }
-
-    };
-
-    const game = computed(() => {
-      return gender.value === 'male' ? 'Baseball' : 'Softball';
     });
 
-    const getDow = () => {
-      if (!gender.value){
-        playingAgeResult.value = "Please select a gender."
-        return;
-      }
+    const getLeagueDowText = (pick) => {
+      const scheduleMap = {
+        tb_one:
+          "1 game weekly on Saturday mornings, potentially some Sunday mid-afternoon",
+        tb_two: "2 games weekly on Monday, Wednesday, or Friday",
+      };
 
-      resultText.value = getLeagueDowText(pick.value, gender.value);
+      const twoGamesMWF = [
+        "10u_baseball",
+        "14u_baseball",
+        "10u_softball",
+        "17u_softball",
+      ];
+      const twoGamesTWT = [
+        "8u_baseball",
+        "12u_baseball",
+        "18u_baseball",
+        "8u_softball",
+        "12u_softball",
+      ];
+
+      if (twoGamesMWF.includes(pick))
+        return "2 games weekly on Monday, Wednesday, or Friday";
+      if (twoGamesTWT.includes(pick))
+        return "2 games weekly on Tuesday, Wednesday, or Thursday";
+
+      return scheduleMap[pick] || "";
+    };
+
+    const getDow = (pickVal) => {
+      pick.value = pickVal;
+      resultText.value = getLeagueDowText(pickVal);
+      return;
     };
 
     return {
       gender,
       pick,
-      resultText
+      resultText,
+      getDow,
     };
   },
 };
@@ -91,15 +165,5 @@ export default {
 #app {
   font-family: Arial, sans-serif;
   margin: 20px;
-}
-
-#birthdateForm {
-  max-width: 700px;
-}
-
-.playingAgeResult {
-  margin-top: 10px;
-  color: #2c3e50;
-  font-weight: bold;
 }
 </style>
